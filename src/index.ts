3356432990a11091377b145f5c1c675f67ca3d65
Matt McKenna <matt@mtmckenna.com>
2022-08-03 22:05:18 -0700

    Remove paths from tsconfig

--- src/index.ts
+++ src/index.ts
@@ -17,78 +17,78 @@ const image = new Image();
 image.src = croissantImage;
 
 const player: pos = {
-	x: width / 2,
-	y: height / 2
+  x: width / 2,
+  y: height / 2,
 };
 
 const inputState: InputState = {
-	left: false,
-	right: false,
-	up: false,
-	down: false
+  left: false,
+  right: false,
+  up: false,
+  down: false,
 };
 
 function tick(t: number) {
-	requestAnimationFrame(tick);
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	if (inputState.left) player.x -= MOVING_SPEED;
-	if (inputState.right) player.x += MOVING_SPEED;
-	if (inputState.up) player.y -= MOVING_SPEED;
-	if (inputState.down) player.y += MOVING_SPEED;
-	player.x = clamp(player.x, 0, canvas.width - 16);
-	player.y = clamp(player.y, 0, canvas.height - 16);
+  requestAnimationFrame(tick);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  if (inputState.left) player.x -= MOVING_SPEED;
+  if (inputState.right) player.x += MOVING_SPEED;
+  if (inputState.up) player.y -= MOVING_SPEED;
+  if (inputState.down) player.y += MOVING_SPEED;
+  player.x = clamp(player.x, 0, canvas.width - 16);
+  player.y = clamp(player.y, 0, canvas.height - 16);
 
-	ctx.drawImage(image, player.x, player.y);
+  ctx.drawImage(image, player.x, player.y);
 }
 
 requestAnimationFrame(tick);
 
 window.addEventListener("keydown", (e: KeyboardEvent) => {
-	switch (e.key) {
-		case "ArrowLeft":
-			inputState.left = true;
-			break;
-		case "ArrowRight":
-			inputState.right = true;
-			break;
-		case "ArrowUp":
-			inputState.up = true;
-			break;
-		case "ArrowDown":
-			inputState.down = true;
-			break;
-	}
+  switch (e.key) {
+    case "ArrowLeft":
+      inputState.left = true;
+      break;
+    case "ArrowRight":
+      inputState.right = true;
+      break;
+    case "ArrowUp":
+      inputState.up = true;
+      break;
+    case "ArrowDown":
+      inputState.down = true;
+      break;
+  }
 });
 
 window.addEventListener("keyup", (e: KeyboardEvent) => {
-	switch (e.key) {
-		case "ArrowLeft":
-			inputState.left = false;
-			break;
-		case "ArrowRight":
-			inputState.right = false;
-			break;
-		case "ArrowUp":
-			inputState.up = false;
-			break;
-		case "ArrowDown":
-			inputState.down = false;
-			break;
-	}
+  switch (e.key) {
+    case "ArrowLeft":
+      inputState.left = false;
+      break;
+    case "ArrowRight":
+      inputState.right = false;
+      break;
+    case "ArrowUp":
+      inputState.up = false;
+      break;
+    case "ArrowDown":
+      inputState.down = false;
+      break;
+  }
 });
 
 function clamp(num: number, min: number, max: number): number {
-	return Math.min(Math.max(num, min), max);
+  return Math.min(Math.max(num, min), max);
 }
 
 interface InputState {
-	left: boolean;
-	right: boolean;
-	up: boolean;
-	down: boolean;
+  left: boolean;
+  right: boolean;
+  up: boolean;
+  down: boolean;
 }
 
 interface pos {
-	x: number;
-	y: number;
+  x: number;
+  y: number;
 }

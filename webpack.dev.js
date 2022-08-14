dbc2b8b925116612b65d877fa03a04d6d04a0b7c
Matt McKenna <matt@mtmckenna.com>
2022-08-13 19:38:12 -0700

    Add sourcemaps to dev

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,6 +3,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
+  devtool: "eval-source-map",
   devServer: {
 		contentBase: './dist',
 		hot: false,
